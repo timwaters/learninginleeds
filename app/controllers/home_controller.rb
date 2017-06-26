09fbd0d4eb9544ba36a837e535290b883f0c4c6e
Tim Waters <chippy2005@gmail.com>
2017-06-26 19:34:23 +0100

    slightly tweak responsive ness of front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     # get recommendations 
     # get topics
     @news = News.all.order(updated_at: :desc).limit(3)
-    @topics =  Topic.all.select { | t | t.has_courses? }
+    @topics =  Topic.all.limit(6).select { | t | t.has_courses? }
   end
 
 

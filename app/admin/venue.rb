ea5b141cbf4c41882882b8ce8da0707e1a6a2b82
Tim Waters <chippy2005@gmail.com>
2017-07-03 00:30:43 +0100

    update course latlon when updating venue location

--- app/admin/venue.rb
+++ app/admin/venue.rb
@@ -1,5 +1,47 @@
 ActiveAdmin.register Venue do
-  permit_params :name, :postcode, :area, :committee, :ward, :latitude, :longitude, :address_1, :address_2, :address_3
+  permit_params :name, :postcode, :latitude, :longitude, :address_1, :address_2, :address_3
+
+
+  controller do
+     def update(options={}, &block)
+
+      super do |success, failure| 
+       
+        success.html { 
+         
+          if resource.saved_changes["latitude"] || resource.saved_changes["longitude"]
+            resource.courses.each do | course |
+              course.latitude = resource.latitude
+              course.longitude = resource.longitude
+              course.lonlat = "POINT(#{resource.longitude} #{resource.latitude})" unless resource.longitude.nil?
+              course.save
+            end
+          end
+
+          redirect_to(admin_venue_path(:venue_id => resource.id))
+         }
+         
+        failure.html { render :edit }
+      end
+
+     end
+  end
+
+
+  form do | f| 
+
+    f.inputs do
+      f.input :name
+      f.input :address_1
+      f.input :address_2
+      f.input :address_3
+      f.input :postcode
+      f.input :latitude
+      f.input :longitude
+    end
+
+    f.actions
+  end
 
   index do
     selectable_column

5dc4a82b7fa43a52a370f369eb89f74efbfcf46a
Tim Waters <chippy2005@gmail.com>
2020-11-11 18:40:01 +0000

    Do not show map for online only courses

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,5 +2,9 @@ class Venue < ApplicationRecord
   has_many :courses
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :postcode, presence: true
+
+  def is_online?
+    postcode == "ZZ99 9ZZ"
+  end
   
 end

59cb928377f19adf0c389e1e3ccf761bb794e8ce
Tim Waters <chippy2005@gmail.com>
2023-10-06 17:49:40 +0100

    add link back to igl site if user comes from there initially

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   self.page_cache_directory = "#{Rails.root}/public/cached_pages"
   protect_from_forgery with: :exception
+  # before_action :test_referer
 
+  # def test_referer
+  #   request.env['HTTP_REFERER'] = 'https://www.inclusivegrowthleeds.com/case-studies-0'
+  # end
   def self.expire_about
     expire_page('about.html')
   end

c017867ff1dacd534cce481a06cf7468a6f2e34f
Tim Waters <chippy2005@gmail.com>
2026-07-31 11:06:19 +0100

    use mini magick to process images

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.active_storage.variant_processor = :mini_magick
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
